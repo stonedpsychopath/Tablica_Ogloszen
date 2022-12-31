2599dd5b4e0c42b61928876efef809ca3940a8cb
stonedpsychopath <112948992+stonedpsychopath@users.noreply.github.com>
2022-12-31 02:35:15 +0100

    ŚWIEŻUTKIE NOWIUTKIE

--- Projekt_TO/Projekt_TO/urls.py
+++ Projekt_TO/Projekt_TO/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from Projekt_TO.views import home_view
+
 urlpatterns = [
+   path('', home_view),
    path('admin/', admin.site.urls),
    path('tablica_ogloszen/', include('tablica_ogloszen.urls', namespace='tablica_ogloszen')),
 ]
