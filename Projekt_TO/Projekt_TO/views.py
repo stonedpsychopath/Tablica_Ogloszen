ffbc3b41ac4cdf627d2a1dd27955db202dee73bf
stonedpsychopath <112948992+stonedpsychopath@users.noreply.github.com>
2023-01-02 11:02:30 +0100

    Delete Projekt_TO directory

--- Projekt_TO/Projekt_TO/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def home_view(request):
-   print(request.user)
-   return render(request, 'home.html')
--- Projekt_TO/tablica_ogloszen/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TablicaOgloszenConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tablica_ogloszen'
